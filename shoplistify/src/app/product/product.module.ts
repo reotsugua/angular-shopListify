b69ce04b6e63cd33cf8341348683fac36071c052
Rosangela <roseffc@gmail.com>
2023-09-21 18:32:28 -0300

    refactor: component product-list e add paginator

--- shoplistify/src/app/product/product.module.ts
+++ shoplistify/src/app/product/product.module.ts
@@ -4,6 +4,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductModalComponent } from './product-modal/product-modal.component';
 import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 
@@ -15,7 +16,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatPaginatorModule
   ],
   exports: [
     ProductListComponent
