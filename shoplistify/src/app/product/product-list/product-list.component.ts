1392f1b5d06004375cfcd12455f3d635a9266d96
Rosangela <roseffc@gmail.com>
2023-09-22 19:41:07 -0300

    style: component product-list

--- shoplistify/src/app/product/product-list/product-list.component.ts
+++ shoplistify/src/app/product/product-list/product-list.component.ts
@@ -6,16 +6,19 @@ import { ProductModalComponent } from '../product-modal/product-modal.component'
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
   products: any[] = [];
 
-  constructor(private productService: ProductService, private dialog: MatDialog) {}
+  constructor(
+    private productService: ProductService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     // pageSize 10, pageNumber 1 e busca vazia
-    this.productService.getProducts('15', '1', '').subscribe((data: any[]) => {
+    this.productService.getProducts('12', '1', '').subscribe((data: any[]) => {
       this.products = this.productService.products = data;
     });
   }
@@ -25,18 +28,22 @@ export class ProductListComponent {
       data: product,
       panelClass: 'product-modal',
       height: '80vh',
-      width: '80vw'
+      width: '80vw',
     });
 
-    productModalDialog.afterClosed().subscribe(result => {
-      if (result.arrowPressed === "left") {
+    productModalDialog.afterClosed().subscribe((result) => {
+      if (result.arrowPressed === 'left') {
         this.handleLeftKeyPress(result);
-      } else if (result.arrowPressed === "right") {
+      } else if (result.arrowPressed === 'right') {
         this.handleRightKeyPress(result);
       } else {
-        this.handleAddProductToCart(result.quantity, result.id, result.wasAdded);
+        this.handleAddProductToCart(
+          result.quantity,
+          result.id,
+          result.wasAdded
+        );
       }
-    })
+    });
   }
 
   private handleRightKeyPress(result: any): void {
@@ -53,7 +60,11 @@ export class ProductListComponent {
     this.openProductModal(productToOpen);
   }
 
-  private handleAddProductToCart(quantity: number, productId: string, wasAdded: boolean): void {
+  private handleAddProductToCart(
+    quantity: number,
+    productId: string,
+    wasAdded: boolean
+  ): void {
     if (wasAdded) {
       //search for productId in cart, add quantity to it
     } else {
@@ -61,26 +72,23 @@ export class ProductListComponent {
     }
     this.getProducts('15', '0');
   }
-  
-  // length:100
-  // pageIndex:1
-  // pageSize:10
-  // previousPageIndex:0
 
-  handlePageEvent(event:any){
-    const {pageIndex, pageSize} = event;
+  handlePageEvent(event: any) {
+    const { pageIndex, pageSize } = event;
     this.getProducts(pageSize, pageIndex);
-    console.log(pageIndex)
+    console.log(pageIndex);
   }
 
-  getProducts( pageSize:string,  pageNumber:string) {
-      // pageSize 10, pageNumber 1 e busca vazia
-      if (pageNumber == '0') {
-        pageNumber = "1"
-      }
-      this.productService.getProducts(pageSize, pageNumber , '').subscribe((data: any[]) => {
-      this.products = data;
-      console.log(data)
-    });
+  getProducts(pageSize: string, pageNumber: string) {
+    // pageSize 10, pageNumber 1 e busca vazia
+    if (pageNumber == '0') {
+      pageNumber = '1';
+    }
+    this.productService
+      .getProducts(pageSize, pageNumber, '')
+      .subscribe((data: any[]) => {
+        this.products = data;
+        console.log(data);
+      });
   }
 }
