ec9620e4330be1f891195f20e1a02b5211c74f56
Mateus Bullerjahn <mateusfb271@gmail.com>
2023-09-23 01:38:27 -0300

    implementada funcionalidade de adicionar produtos ao carrinho, aparecendo a quantidade na badge. Está sendo salvo no local storage

--- shoplistify/src/app/app.component.ts
+++ shoplistify/src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component, HostBinding, effect, signal } from '@angular/core';
+import { Component, HostBinding, OnInit, effect, signal } from '@angular/core';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public totalProductQuantity: number;
+
+  constructor(private productService: ProductService) {
+    effect(() => {
+      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
+    });
+  }
+
+  ngOnInit(): void {
+    this.productService.productsTotal$.subscribe((productsTotal) => {
+      this.totalProductQuantity = productsTotal;
+    })
+  }
+
   title = 'shoplistify';
   darkMode = signal<boolean>(
     JSON.parse(window.localStorage.getItem('darkMode') ?? 'false')
@@ -13,11 +28,5 @@ export class AppComponent {
 
   @HostBinding('class.dark') get mode() {
     return this.darkMode();
-  }
-
-  constructor() {
-    effect(() => {
-      window.localStorage.setItem('darkMode', JSON.stringify(this.darkMode()));
-    });
   }
 }
--- shoplistify/src/app/product/product-list/product-list.component.ts
+++ shoplistify/src/app/product/product-list/product-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ProductService } from 'src/app/services/product.service';
 import { ProductModalComponent } from '../product-modal/product-modal.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +11,13 @@ import { ProductModalComponent } from '../product-modal/product-modal.component'
 })
 export class ProductListComponent {
   products: any[] = [];
+  productsInCartArray: {productId: string, productQuantity: number}[] = [];
+  productsQuantityInCart: number = 0;
 
   constructor(
     private productService: ProductService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -21,6 +25,16 @@ export class ProductListComponent {
     this.productService.getProducts('12', '1', '').subscribe((data: any[]) => {
       this.products = this.productService.products = data;
     });
+
+    if (localStorage.getItem("products")) {
+      this.productsInCartArray = JSON.parse(localStorage.getItem("products")!);
+      this.productsInCartArray.forEach(product => {
+        this.productsQuantityInCart += product.productQuantity;
+        this.productService.productsTotal$.next(this.productsQuantityInCart); 
+      })
+    } else {
+      this.productService.productsTotal$.next(0);
+    }
   }
 
   public openProductModal(product: any): void {
@@ -37,7 +51,7 @@ export class ProductListComponent {
       } else if (result.arrowPressed === 'right') {
         this.handleRightKeyPress(result);
       } else {
-        this.handleAddProductToCart(
+        this.updateCart(
           result.quantity,
           result.id,
           result.wasAdded
@@ -60,26 +74,104 @@ export class ProductListComponent {
     this.openProductModal(productToOpen);
   }
 
-  private handleAddProductToCart(
+  private updateCart(
     quantity: number,
     productId: string,
     wasAdded: boolean
   ): void {
     if (wasAdded) {
-      //search for productId in cart, add quantity to it
+      this.handleAddProductToCart(productId, quantity);
     } else {
-      //search for productId in cart, subtract quantity from it
+      this.handleRemoveProductFromCart(productId, quantity);
     }
-    this.getProducts('15', '0');
+
+    this.manageCartProducts();
   }
 
-  handlePageEvent(event: any) {
+  private handleAddProductToCart(productId: string, quantity: number): void {
+    if (this.productIsAlreadyOnCart(productId)) {
+      this.updateProductQuantityInCart(productId, quantity);
+    } else {
+      this.addNewProductToCart(productId, quantity);
+    }
+
+    this.productsQuantityInCart += quantity;
+  }
+
+  private updateProductQuantityInCart(productId: string, quantity: number): void {
+    let productIndexInCartArray = this.productsInCartArray.findIndex(product => product.productId === productId);
+    this.productsInCartArray[productIndexInCartArray].productQuantity += quantity;
+    this.snackBar.open("Quantidade do produto alterada com sucesso!", '', {
+      duration: 2000
+    });
+  }
+
+  private addNewProductToCart(productId: string, quantity: number): void {
+    this.productsInCartArray.push({ productId: productId, productQuantity: quantity });
+    this.snackBar.open("Produto adicionado ao carrinho com sucesso!", '', {
+      duration: 2000
+    });
+  }
+
+  private handleRemoveProductFromCart(productId: string, quantity: number): void {
+    if (this.productIsAlreadyOnCart(productId)) {
+      this.removeItemOrDecreaseQuantity(productId, quantity);
+    } else {
+      this.snackBar.open("Você não tinha nenhuma unidade deste produto em seu carrinho!", '', {
+        duration: 2000
+      });
+    }
+  }
+
+  private removeItemOrDecreaseQuantity(productId: string, quantity: number): void {
+    let productIndexInCartArray = this.getProductIndexInCartArray(productId);
+    let productQuantityInCart = this.getProductQuantityInCart(productIndexInCartArray);
+
+    if ( productQuantityInCart <= quantity) {
+      this.removeItemFromCart(productIndexInCartArray);
+    } else {
+      this.decreaseItemQuantityInCart(productIndexInCartArray, quantity);
+    }
+  }
+
+  private getProductIndexInCartArray(productId: string): number {
+    return this.productsInCartArray.findIndex(product => product.productId === productId);
+  }
+
+  private getProductQuantityInCart(productIndexInCartArray: number): number {
+    return this.productsInCartArray[productIndexInCartArray].productQuantity;
+  }
+
+  private removeItemFromCart(productIndexInCartArray: number): void {
+    this.productsInCartArray.splice(productIndexInCartArray, 1);
+    this.snackBar.open("Todas as unidades do produto foram removidas do seu carrinho!", '', {
+      duration: 2000
+    });
+    this.productsQuantityInCart -= this.productsInCartArray[productIndexInCartArray].productQuantity;
+  }
+
+  private decreaseItemQuantityInCart(productIndexInCartArray: number, quantity: number): void {
+    this.productsInCartArray[productIndexInCartArray].productQuantity -= quantity;
+    this.productsQuantityInCart -= quantity;
+  }
+
+  private productIsAlreadyOnCart(productId: string): { productId: string, productQuantity: number } | undefined {
+    return this.productsInCartArray.find(product => product.productId === productId);
+  }
+
+  private manageCartProducts(): void {
+    localStorage.setItem("products", JSON.stringify(this.productsInCartArray));
+    this.productService.productsTotal$.next(this.productsQuantityInCart);
+    this.productService.productsInCartArray$.next(this.productsInCartArray);
+  }
+
+  handlePageEvent(event: any): void {
     const { pageIndex, pageSize } = event;
     this.getProducts(pageSize, pageIndex);
     console.log(pageIndex);
   }
 
-  getProducts(pageSize: string, pageNumber: string) {
+  getProducts(pageSize: string, pageNumber: string): void {
     // pageSize 10, pageNumber 1 e busca vazia
     if (pageNumber == '0') {
       pageNumber = '1';
